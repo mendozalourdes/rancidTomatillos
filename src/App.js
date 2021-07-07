8accc10b48584d97d032cc2fc97b63a6dbbbef77
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-07-07 16:46:39 -0600

    Add h1 element

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends React.Component {
     render() {
       return (
         <main>
+          <h1>Rancid Tomatillos</h1>
           {/* This is where we will import Movies component */}
           {/* this is where our conditional rendering will happen */}
           {/* selected movie within conditional */}
