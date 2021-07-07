684f52d2a12726d0412a2edfa82cf54d2704fb69
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-07-07 16:58:40 -0600

    Add MoviesRepo basic structure

--- src/components/MoviesRepo/MoviesRepo.js
+++ src/components/MoviesRepo/MoviesRepo.js
@@ -1,5 +1,23 @@
 import React from "react"
 
+class MoviesRepo extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            movies: props.movies
+        }
+    }
 
+    render() {
+        const movieComponents = this.state.movies.map(movie => {
+            return <MovieCard />
+        })
+        return (
+            <div>
+                {movieComponents}
+            </div>
+        )
+    }
+}
 
 export default MoviesRepo
