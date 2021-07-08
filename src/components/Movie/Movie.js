059a5215c5670c762ab15e16d7270b21a98a4fae
lourdesmendoza <lourd.mendoza@gmail.com>
2021-07-07 19:56:12 -0500

    Add styling to movie component and repo component

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,11 +1,11 @@
 import React from "react"
+import './Movie.css'
 
 const Movie = (props) => {
     const {title, image} = props
     return(
         <div>
-            <img src={image}/>
-            <h2>{title}</h2>
+            <img className="movie-poster" src={image}/>
         </div>
     )
 }
