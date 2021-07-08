3670b3c1c0e31973d08bcf48081f30c0f574855b
lourdesmendoza <lourd.mendoza@gmail.com>
2021-07-07 20:39:32 -0500

    Add styling to background and title

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -4,10 +4,11 @@ import './Movie.css'
 const Movie = (props) => {
     const {title, image} = props
     return(
-        <div>
+        <div className="poster-container">
             <img className="movie-poster" src={image}/>
+            <button>More Info</button>
         </div>
     )
 }
 
-export default Movie
+export default Movie;
