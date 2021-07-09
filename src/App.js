637c36e6abf352844444e56ac725f9160e009ab0
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-07-08 18:09:43 -0600

    Add Selected movie directory and files, import to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 import movieData from "./movieData"
 import React from "react"
 import MoviesRepo from "./components/MoviesRepo/MoviesRepo"
+import SelectedMovie from "./components/SelectedMovie/SelectedMovie"
 
 class App extends React.Component {
     constructor() {
---
+++ src/components/SelectedMovie/SelectedMovie.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const SelectedMovie = (props) => {
+    return (
+        <div className="selected-movie">
+
+        </div>
+    )
+}
+
+export default SelectedMovie
