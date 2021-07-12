62636b4a1b2d635490efda9d0340e5f1ff5e766f
lourdesmendoza <lourd.mendoza@gmail.com>
2021-07-12 08:28:13 -0600

    Adjust styling for selected movie view

--- src/components/SelectedMovie/SelectedMovie.js
+++ src/components/SelectedMovie/SelectedMovie.js
@@ -5,13 +5,14 @@ const SelectedMovie = (props) => {
     const { poster, backdrop, title, rating, releaseDate, budget, revenue, runtime, overview, tagline} = props
     return (
         <section className="selected-movie">
+              <h3 className="movie-title">{title}</h3>
             <div className="sidebar-backdrop-container">
              <aside className="sidebar">
-                <h3>{releaseDate}</h3>
-                <h3>{rating}</h3>
-                <h3>{budget}</h3>
-                <h3>{revenue}</h3>
-                <h3>{runtime}</h3>
+                <h3>Released: {releaseDate}</h3>
+                <h3>Rating: {rating}</h3>
+                <h3>Budget: {budget}</h3>
+                <h3>Revenue: {revenue}</h3>
+                <h3>Runtime: {runtime}</h3>
             </aside>
             <div className="backdrop-container">
                     <img src={backdrop} alt={title} className="backdrop"/>
@@ -19,9 +20,13 @@ const SelectedMovie = (props) => {
             </div>
             <div className="poster-container">
                 <div className="movie-info">
-                    <h3>{title}</h3>
+                    {/* <h3 className="movie-title">{title}</h3> */}
                 <article className="overview">
+                    <h3>{tagline}</h3>
+                    
                     <h3>{overview}</h3>
+                    <section className="title-tagline">
+            </section>
                 </article>
                 </div>
                 <div>
@@ -29,20 +34,13 @@ const SelectedMovie = (props) => {
                 </div>
                 
             </div>
-            <aside className="sidebar">
-                <h3>{releaseDate}</h3>
-                <h3>{rating}</h3>
-                {/* <h3>{budget}</h3>
-                <h3>{revenue}</h3>
-                <h3>{runtime}</h3> */}
-            </aside>
             {/* <article className="genres">
                 <h3>{genres}</h3>
             </article> */}
-            <section className="title-tagline">
-                <h3>{tagline}</h3>
-            </section>
-            <button onClick={() => props.returnHome()}>Return Home</button>
+            {/* <section className="title-tagline">
+                <h3>Tagline: {tagline}</h3>
+            </section> */}
+            <button className="border" onClick={() => props.returnHome()}>Return Home</button>
         </section>
     )
 }
