1a76f74dcb557d8f65d286eabc573a19eb772dc0
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-07-13 15:08:11 -0600

    Write custom command for beforeEach in SelectedMovieView

--- cypress/integration/selectedMovieView_spec.js
+++ cypress/integration/selectedMovieView_spec.js
@@ -1,81 +1,82 @@
 // Describe(“User clicks on a movie to learn more details”) aka Selected Movie view
+//import fixture here
+
 describe("Selected Movie View", () => {
     beforeEach(() => {
+        cy.loadMain()
         //how to connect to fixture??
         //cy.intercept('GET', '/movies', { fixture: 'movies.json' }).as("movies")
+        //connect fixture, visit page, intercept fetch for all movies
+        //click on button
 
         //visit site
-        cy.visit('http://localhost:3000');
+        // cy.visit('http://localhost:3000');
+        //intercept fetch for all movies here
+        //then click on it to find
 
         //mock click on button
-        cy.get("#1")
-            .click().should("include", "/1")
+        // cy.get("#1")
+        //     .click().should("include", "/1")
 
         //intercept click on movie detail to route to correct movie id file path
         // happy path 201
-            cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v2/movies/1", {
-                movie: {
-                    id: 1, 
-                    title: "Fake Movie Title", 
-                    poster_path: "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg", 
-                    backdrop_path: "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg", 
-                    release_date: "2019-12-04", 
-                    overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!", 
-                    average_rating: 6, 
-                    genres: ["Drama"], 
-                    budget:63000000, 
-                    revenue:100853753, 
-                    runtime:139, 
-                    tagline: "It's a movie!" 
-                }
-            })
+            // cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v2/movies/1", {
+            //     statusCode: 200,
+            //     body: {
+            //         movie: {
+            //             id: 1, 
+            //             title: "Fake Movie Title", 
+            //             poster_path: "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg", 
+            //             backdrop_path: "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg", 
+            //             release_date: "2019-12-04", 
+            //             overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!", 
+            //             average_rating: 6, 
+            //             genres: ["Drama"], 
+            //             budget:63000000, 
+            //             revenue:100853753, 
+            //             runtime:139, 
+            //             tagline: "It's a movie!" 
+            //         }
+            //     }
+            // })
+            //mock network responses for errors?
     });
 // cy.intercept({}) - sad path 400s
-    it("should alert the user of a 404 error", () => {
-        cy.get("#1")
-            .click().should("include", "/1")
-        cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v2movies/1", {
-                movie: {
-                    id: 1, 
-                    title: "Fake Movie Title", 
-                    poster_path: "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg", 
-                    backdrop_path: "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg", 
-                    release_date: "2019-12-04", 
-                    overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!", 
-                    average_rating: 6, 
-                    genres: ["Drama"], 
-                    budget:63000000, 
-                    revenue:100853753, 
-                    runtime:139, 
-                    tagline: "It's a movie!" 
-                }
-            })
-        cy.get("h2")
-            .contains("Something went wrong. Please try again later.")
-    })
+    // it("should alert the user of a 404 error", () => {
+    //     cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v2movies/1", {
+    //         statusCode: 404,
+    //         body: {
+    //             id: 2,
+    //             image: "https://ca.slack-edge.com/T029P2S9M-U37MJAV0T-007ccf2f5eb2-512",
+    //             name: "Leta Keane"
+    //         }
+    //         })
+    //     cy.get("h2")
+    //         .contains("Something went wrong. Please try again later.")
+    // })
 // cy.intercept({}) - sad path 500s
-    it("should alert the user of a 500 error", () => {
-        cy.get("#1")
-            .click().should("include", "/1")
-        cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v1/movies/1", {
-                movie: {
-                    id: 1, 
-                    title: "Fake Movie Title", 
-                    poster_path: "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg", 
-                    backdrop_path: "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg", 
-                    release_date: "2019-12-04", 
-                    overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!", 
-                    average_rating: 6, 
-                    genres: ["Drama"], 
-                    budget:63000000, 
-                    revenue:100853753, 
-                    runtime:139, 
-                    tagline: "It's a movie!" 
-                }
-            })
-        cy.get("h2")
-            .contains("Our servers are currently down. Please try again.")
-    })
+    // it("should alert the user of a 500 error", () => {
+    //     cy.get("#1")
+    //         .click().should("include", "/1")
+    //     cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v1/movies/1", {
+    //             movie: {
+    //                 id: 1, 
+    //                 title: "Fake Movie Title", 
+    //                 poster_path: "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg", 
+    //                 backdrop_path: "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg", 
+    //                 release_date: "2019-12-04", 
+    //                 overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!", 
+    //                 average_rating: 6, 
+    //                 genres: ["Drama"], 
+    //                 budget:63000000, 
+    //                 revenue:100853753, 
+    //                 runtime:139, 
+    //                 tagline: "It's a movie!" 
+    //             }
+    //         })
+    //     cy.get("h2")
+    //         .contains("Our servers are currently down. Please try again.")
+    // })
 // cy.intercept({}) - loading 
     // it("should tell user when the page is loading", () => {
     //     cy.get("#1")
@@ -108,7 +109,7 @@ describe("Selected Movie View", () => {
 
     it("should show the user the movie they selected with movie details from the database", () => {
         //backdrop
-        cy.get(".backdrop-container")
+        cy.get('.backdrop')
             .find("img")
             .should("be.visible")
 
@@ -151,9 +152,9 @@ describe("Selected Movie View", () => {
             .should("not.exist")
     })
 
-    it("should have a URL path", () => {
-        cy.url().should("include", "/1")
-    })
+    // it("should have a URL path", () => {
+    //     cy.url().should("include", "/1")
+    // })
 
     it("should have a return home button", () => {
         cy.get("button")
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -22,4 +22,37 @@
 //
 //
 // -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... }
+
+
+Cypress.Commands.add('loadMain', () => {
+    cy.fixture('movies.json').as('movies')
+
+    const baseURL = "https://rancid-tomatillos.herokuapp.com/api/v2"
+
+    cy.intercept(`${baseURL}/movies`, {
+        fixture: "movies"
+    });
+
+    cy.intercept(`${baseURL}/1`, {
+        movie: {
+                "id": 1,  
+                "title": "Fake Movie Title", 
+                "poster_path": "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg", 
+                "backdrop_path": "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg", 
+                "release_date": "2019-12-04", "overview": "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!", 
+                "average_rating": 6, 
+                "genres": ["Drama"], 
+                "budget": 63000000, 
+                "revenue": 100853753, 
+                "runtime": 139, 
+                "tagline": "It's a movie!" 
+            }
+    });
+
+    cy.visit('http://localhost:3000')
+        .get("#1")
+        .click()
+        //.should("include", "/1")
+
+});
