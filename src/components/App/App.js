a4cee2b35fcb153a39f751b7097fd0ef1a51df3a
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-07-15 17:41:17 -0600

    Add basic styling for randomMovieBackdrop

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -17,15 +17,16 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-    apiCalls.fetchAPIData("/movies")
-    .then(response => {
-      if(typeof response === 'string') {
-        this.setState({ error: response })
-      } else {
-        this.setState({movies: response.movies})
-      }
-    })
-    .catch(err => err.message)
+      apiCalls.fetchAPIData("/movies")
+        .then(response => {
+          if(typeof response === 'string') {
+            this.setState({ error: response })
+          } else {
+            this.setState({movies: response.movies})
+          }
+          console.log(this.state.movies[0].backdrop_path)
+      })
+      .catch(err => err.message)
 }
 
 
@@ -49,18 +50,23 @@ class App extends React.Component {
     render() {
       return (
         <main>
-            <header>
-              <h1 className="app-title">Rancid Tomatillos</h1>
-            </header>
+          <header>
+            <h1 className="app-title">Rancid Tomatillos</h1>
+          </header>
+          <section className="random-movie-section">
+            {/* this is where our RandomMovie component will go */}
+            
+            <img className="random-movie-backdrop" src="https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg"/>
+          </section>
           {this.state.error && <h2>{this.state.error}</h2>}
-            <Route exact path="/" >
+          <Route exact path="/" >
               <MoviesRepo movies={this.state.movies} showMovieDetails={this.showMovieDetails}/> 
-            </Route>
+          </Route>
 
-            <Route
-              path="/movies/:id" render={() => {
-                return <SelectedMovie {...this.state.selectedMovie} returnHome={this.returnHome} />
-              }}/>
+          <Route
+            path="/movies/:id" render={() => {
+            return <SelectedMovie {...this.state.selectedMovie} returnHome={this.returnHome} />}
+          }/>
         </main>
       );
     }
