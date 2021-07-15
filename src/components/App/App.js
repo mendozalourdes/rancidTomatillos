ca6f4dd9c5d79c6ff845f19a8da81fbd6b2b39c1
lourdesmendoza <lourd.mendoza@gmail.com>
2021-07-15 17:36:27 -0600

    Add Loading movies message for error handling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -43,7 +43,6 @@ class App extends React.Component {
         this.setState({selectedMovie: response.movie})
       }
     })
-      // .then(selectedMovie => this.setState({selectedMovie: selectedMovie.movie}))
       .catch(err => err.message)
     } 
 
@@ -53,43 +52,19 @@ class App extends React.Component {
             <nav>
               <h1 className="app-title">Rancid Tomatillos</h1>
             </nav>
+          {!this.state.movies.length && !this.state.error.length &&
+        <h2> Loading movies...</h2> }
           {this.state.error && <h2>{this.state.error}</h2>}
-          {/* selected movie within conditional */}
             <Route exact path="/" >
               <MoviesRepo movies={this.state.movies} showMovieDetails={this.showMovieDetails}/> 
             </Route>
 
             <Route
               path="/movies/:id" render={() => {
-                // const chosenMovie = this.state.movies.find(movie => movie.id === parseInt(match.params.id)) 
-                // console.log("chosen", match)
-                // console.log("state", this.state.movies)
-                // console.log("select", this.state.selectedMovie)
-                // console.log("chosen", chosenMovie)
                 return <SelectedMovie {...this.state.selectedMovie} returnHome={this.returnHome} />
               }}/>
 
 
-{/* 
-          {this.state.movies && this.state.selectedMovie ? <SelectedMovie 
-                                        key={this.state.selectedMovie.id} 
-                                        poster={this.state.selectedMovie.poster_path} 
-                                        backdrop={this.state.selectedMovie.backdrop_path}
-                                        title={this.state.selectedMovie.title}
-                                        rating={this.state.selectedMovie.average_rating}
-                                        releaseDate={this.state.selectedMovie.release_date}
-                                        budget={this.state.selectedMovie.budget}
-                                        revenue={this.state.selectedMovie.revenue}
-                                        tagline={this.state.selectedMovie.tagline}
-                                        genres={this.state.selectedMovie.genres}
-                                        overview={this.state.selectedMovie.overview}
-                                        returnHome={this.returnHome}
-                                      />
-                                    : <MoviesRepo 
-                                        movies={this.state.movies} 
-                                        showMovieDetails={this.showMovieDetails}
-                                      /> 
-          }                          */}
         </main>
       );
     }
