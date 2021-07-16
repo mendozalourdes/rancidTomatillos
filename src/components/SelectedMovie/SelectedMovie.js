57891a409d1a0f126ec056ea667786e44a15d262
lourdesmendoza <lourd.mendoza@gmail.com>
2021-07-16 13:11:59 -0600

    Write render method in selectedMovie file

--- src/components/SelectedMovie/SelectedMovie.js
+++ src/components/SelectedMovie/SelectedMovie.js
@@ -16,7 +16,7 @@ class SelectedMovie extends Component {
           
         }
     }
-    
+
     componentDidMount() {
         this.setState({selectedMovie: ""})
         apiCalls.fetchAPIData(`/movies/${this.props.match.params.id}`)
@@ -33,7 +33,71 @@ class SelectedMovie extends Component {
   returnHome = () => {
     this.setState({selectedMovie: ""})
   }
+ render() {
 
+          const {
+            poster_path, 
+            backdrop_path, 
+            genres, 
+            title, 
+            average_rating, 
+            release_date, 
+            budget, 
+            revenue, 
+            runtime, 
+            overview, 
+            tagline, 
+            id
+          } = this.state.selectedMovie
+
+        let movieDetails =  
+        <section className="selected-movie">
+        <h3 className="movie-title">{title}</h3>
+        <h4 className="tagline">{tagline}</h4>
+      <div className="sidebar-backdrop-container">
+       <aside className="sidebar">
+          <h3>Released: {release_date}</h3>
+          <h3>Rating: {average_rating}</h3>
+          <h3>Budget: {budget}</h3>
+          <h3>Revenue: {revenue}</h3>
+          <h3>Runtime: {runtime}</h3>
+      </aside>
+      <div className="backdrop-container">
+              <img src={backdrop_path} alt={title} className="backdrop"/>
+      </div>
+      </div>
+      <article className="genres">
+          <h3>{genres}</h3>
+      </article>
+      <div className="poster-holder">
+          <div className="movie-info">
+          <article className="overview">
+              <h3>{overview}</h3>
+              <button className="return-home" onClick={() => this.returnHome()}>Return Home</button>
+          </article>
+          </div>
+          <div>
+              <img className="selected-movie-poster" src={poster_path}/>
+          </div>
+      </div>
+  </section>
+
+          return (
+            <section>
+                {!this.state.selectedMovie && !this.state.error.length && 
+                    <h2> Loading Movie Details...</h2> 
+                    }
+           {!this.state.selectedMovie && !this.state.error.length && 
+                <img className="loading-image" src={ loadingImage }></img>
+                }     
+
+        {this.state.error ?  <h2> {this.state.error}</h2> : movieDetails}
+
+            </section>
+
+          )
+      
+  }
 
   
 
