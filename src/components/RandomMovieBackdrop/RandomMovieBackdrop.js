f044f54f0f5c7470fc90597b02bd4b9537c87ce0
lourdesmendoza <lourd.mendoza@gmail.com>
2021-07-17 15:40:12 -0600

    Remove console logs

--- src/components/RandomMovieBackdrop/RandomMovieBackdrop.js
+++ src/components/RandomMovieBackdrop/RandomMovieBackdrop.js
@@ -3,14 +3,9 @@ import React from "react"
 
 
 const RandomMovieBackdrop = (props) => {
-    console.log("gimmePROPS", props)
-    // console.log('propssss', props.movies[0].backdrop_path)
     const randomMovieIndex = Math.floor(Math.random() * props.movies.length)
-    console.log("randooo", randomMovieIndex)
     const randomBackdrop = (props.movies[randomMovieIndex].backdrop_path)
     const randomTitle = (props.movies[randomMovieIndex].title)
-    console.log("gimmmmmeRandoOOOOO", randomBackdrop)
-    // console.log("backdropppp", props.movies[0].backdrop)
 
     return (
         <section className="random-movie-section">
