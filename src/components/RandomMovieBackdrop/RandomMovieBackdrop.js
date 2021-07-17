7c989ed42773ae76ed788c8b5cd3be671b22be91
lourdesmendoza <lourd.mendoza@gmail.com>
2021-07-17 15:12:05 -0600

    Begin random movie code

--- src/components/RandomMovieBackdrop/RandomMovieBackdrop.js
+++ src/components/RandomMovieBackdrop/RandomMovieBackdrop.js
@@ -2,5 +2,20 @@ import React from "react"
 import MoviesRepo from "../MoviesRepo/MovieRepo"
 
 
+const RandomMovieBackdrop = props => {
+    console.log("gimmePROPS", props)
+    console.log('propssss', props.movies[0].backdrop_path)
+    const randomMovieIndex = Math.floor(Math.random() * props.movies.length)
+    console.log("randooo", randomMovieIndex)
+    const randomBackdrop = (props.movies[randomMovieIndex].backdrop_path)
+    console.log("gimmmmmeRandoOOOOO", randomBackdrop)
+    // console.log("backdropppp", props.movies[0].backdrop)
+    })
+    return (
+            <section className="random-movie-section">
+                <img className="random-movie-backdrop" src={randomBackdrop}/>
+            </section>
+        )
+}
 
 export default RandomMovieBackdrop
