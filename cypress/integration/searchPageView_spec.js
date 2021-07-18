e8f1a9440c04221235998bfdaeffa65a0f69a0e3
lourdesmendoza <lourd.mendoza@gmail.com>
2021-07-18 14:09:55 -0600

    Create tests for error handling when user clicks on buttons

--- cypress/integration/searchPageView_spec.js
+++ cypress/integration/searchPageView_spec.js
@@ -20,6 +20,7 @@ it('should be able to search for a movie given an input', () => {
     cy.get('input').type('Bill')
       .get('.movie-container').children().should('have.length', 1)
       .get('img').should('have.class', 'movie-poster') 
+      .get('button').contains('More Info')
   })
 
   it('should show no results if input does not match film selection', () => {
@@ -28,14 +29,31 @@ it('should be able to search for a movie given an input', () => {
     
   })
 
-  it('should show an error message if the input does not match film selection', () => {
-    cy.get('input').type('The Simpsons')
-      .get('.movie-container').children().should('have.length', 0)
-    cy.get('h1')
-        .contains("Sorry, we couldn't find any movies to match your search. Please try again!")
+//   it('should show an error message if the input does not match film selection', () => {
+//     cy.get('input').type('The Simpsons')
+//       .get('.movie-container').children().should('have.length', 0)
+//     cy.get('h1')
+//         .contains("Sorry, we couldn't find any movies to match your search. Please try again!")
+    
+//   })
+
+  it('should clear input after user clicks on the more info button', () => {
+    cy.get('input').type('Mulan')
+      .get('.movie-container').children().should('have.length', 1)
+      .get('img').should('have.class', 'movie-poster') 
+      .get('button').contains('More Info').click()
+    cy.get('input').should('be.empty')
     
   })
 
+  it('should display all movies if user clicks on search button with empty input', () => {
+    cy.get('input').should('be.empty')
+    cy.get('form').find('button').click()
+     .location('pathname').should('eq', '/search')
+     .get('.movie-container').children().should('have.length', 41)
+ 
+    
+  })
 
 
     }); 
