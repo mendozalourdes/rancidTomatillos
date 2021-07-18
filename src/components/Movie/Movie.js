e83da09d7e1e48dd4e554fefb496c0e4e8679d3d
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-07-18 14:24:14 -0600

    Fix indentation

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -6,7 +6,7 @@ const Movie = (props) => {
     const { id, image, title } = props
     return(
         <Link to={`/movies/${id}`}>
-        <div className="poster-container" onClick={props.cleanInputs}>
+        <div className="poster-container" onSubmit={props.cleanInputs} onClick={props.cleanInputs}>
             <img className="movie-poster" src={image} alt={`Movie poster for ${title}`}/>
             <div className="button-container">
                 <button className="info-button" id={id}>More Info</button>
--- src/components/SelectedMovie/SelectedMovie.js
+++ src/components/SelectedMovie/SelectedMovie.js
@@ -9,99 +9,88 @@ import { modifyMovieData } from '../../Utilities/Utils'
 
 
 class SelectedMovie extends Component {
-    constructor(props) {
-        super(props);
-        this.state= {
-            selectedMovie: "",
-            error: "",
+  constructor(props) {
+    super(props);
+      this.state= {
+          selectedMovie: "",
+          error: "",
           
-        }
-    }
+      }
+  }
 
-    componentDidMount() {
-        this.setState({selectedMovie: ""})
-        apiCalls.fetchAPIData(`/movies/${this.props.match.params.id}`)
-          .then(response => {
-            if(typeof response === 'string') {
-              this.setState({ error: response })
-            } else {
-              this.setState({selectedMovie: modifyMovieData(response.movie)})
-            }
-        })
-        .catch(err => err.message)
+  componentDidMount() {
+    this.setState({selectedMovie: ""})
+    apiCalls.fetchAPIData(`/movies/${this.props.match.params.id}`)
+      .then(response => {
+        if(typeof response === 'string') {
+          this.setState({ error: response })
+        } else {
+          this.setState({selectedMovie: modifyMovieData(response.movie)})
+        }
+    })
+    .catch(err => err.message)
   }
 
  render() {
+  const {
+    poster_path, 
+    backdrop_path, 
+    genres, 
+    title, 
+    average_rating, 
+    release_date, 
+    budget, 
+    revenue, 
+    runtime, 
+    overview, 
+    tagline
+  } = this.state.selectedMovie
 
-          const {
-            poster_path, 
-            backdrop_path, 
-            genres, 
-            title, 
-            average_rating, 
-            release_date, 
-            budget, 
-            revenue, 
-            runtime, 
-            overview, 
-            tagline
-          } = this.state.selectedMovie
-
-        let movieDetails =  
-        <section className="selected-movie">
-        <h3 className="movie-title">{title}</h3>
-        <h4 className="tagline">{tagline}</h4>
+  let movieDetails =  
+    <section className="selected-movie">
+      <h3 className="movie-title">{title}</h3>
+      <h4 className="tagline">{tagline}</h4>
       <div className="sidebar-backdrop-container">
-       <aside className="sidebar">
+        <aside className="sidebar">
           <h3>Released: {release_date}</h3>
           <h3>Rating: {average_rating} / 10</h3>
           <h3>Budget: ${budget}</h3>
           <h3>Revenue: ${revenue}</h3>
           <h3>Runtime: {runtime} minutes</h3>
-      </aside>
-      <div className="backdrop-container">
-              <img src={backdrop_path} alt={title} className="backdrop"/>
-      </div>
+        </aside>
+        <div className="backdrop-container">
+          <img src={backdrop_path} alt={title} className="backdrop"/>
+        </div>
       </div>
       <article className="genres">
-          <h3>{genres}</h3>
+        <h3>{genres}</h3>
       </article>
       <div className="poster-holder">
-          <div className="movie-info">
+        <div className="movie-info">
           <article className="overview">
-              <h3>{overview}</h3>
+            <h3>{overview}</h3>
           <Link to={"/"}>
-              <button className="return-home">Return Home</button>
+            <button className="return-home">Return Home</button>
           </Link>
           </article>
-          </div>
-          <div>
-              <img className="selected-movie-poster" alt={`A post of the film ${title}`} src={poster_path}/>
-          </div>
+        </div>
+        <div>
+          <img className="selected-movie-poster" alt={`A post of the film ${title}`} src={poster_path}/>
+        </div>
       </div>
-  </section>
+    </section>
 
-          return (
-            <section>
-                {!this.state.selectedMovie && !this.state.error.length && 
-                    <h2> Loading Movie Details...</h2> 
-                    }
-           {!this.state.selectedMovie && !this.state.error.length && 
-                <img className="loading-image" alt={"Loading movie details"} src={ loadingImage }></img>
-                }     
-
-        {this.state.error ?  <h2> {this.state.error}</h2> : movieDetails}
-
-            </section>
-
-          )
-      
-  }
-
-  
-
-
-
+  return (
+    <>
+      {!this.state.selectedMovie && !this.state.error.length && 
+        <h2> Loading Movie Details...</h2> 
+      }
+      {!this.state.selectedMovie && !this.state.error.length && 
+        <img className="loading-image" alt={"Loading movie details"} src={ loadingImage }></img>
+      }     
+      {this.state.error ?  <h2> {this.state.error}</h2> : movieDetails}
+    </>
+  )}
 }
 
 export default SelectedMovie;
