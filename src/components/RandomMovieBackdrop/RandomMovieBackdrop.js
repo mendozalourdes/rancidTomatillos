be91c6594d58de51516991b660ee48d7ba846dc6
lourdesmendoza <lourd.mendoza@gmail.com>
2021-07-19 13:38:35 -0600

    Adjust indendation for RandomMovie file

--- src/components/RandomMovieBackdrop/RandomMovieBackdrop.js
+++ src/components/RandomMovieBackdrop/RandomMovieBackdrop.js
@@ -1,18 +1,20 @@
-import React from "react"
-
-
+import React from "react";
 
 const RandomMovieBackdrop = (props) => {
-    const randomMovieIndex = Math.floor(Math.random() * props.movies.length)
-    const randomBackdrop = (props.movies[randomMovieIndex].backdrop_path)
-    const randomTitle = (props.movies[randomMovieIndex].title)
+  const randomMovieIndex = Math.floor(Math.random() * props.movies.length);
+  const randomBackdrop = props.movies[randomMovieIndex].backdrop_path;
+  const randomTitle = props.movies[randomMovieIndex].title;
 
-    return (
-        <section className="random-movie-section">
-                <h2 className="backdrop-title"> {randomTitle} </h2>
-                <img className="random-movie-backdrop" src={randomBackdrop} alt={`Backdrop from ${randomTitle}`}/>
-            </section>
-        )
-}
+  return (
+    <section className="random-movie-section">
+      <h2 className="backdrop-title"> {randomTitle} </h2>
+      <img
+        className="random-movie-backdrop"
+        src={randomBackdrop}
+        alt={`Backdrop from ${randomTitle}`}
+      />
+    </section>
+  );
+};
 
 export default RandomMovieBackdrop;
