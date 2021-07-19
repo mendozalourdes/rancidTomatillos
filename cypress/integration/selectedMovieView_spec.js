9325a8d8976b5bd7a3a4a6705a7560d16522ac5e
lourdesmendoza <lourd.mendoza@gmail.com>
2021-07-19 13:57:07 -0600

    Remove commented out code

--- cypress/integration/selectedMovieView_spec.js
+++ cypress/integration/selectedMovieView_spec.js
@@ -1,11 +1,8 @@
-// Describe(“User clicks on a movie to learn more details”) aka Selected Movie view
-//import fixture here
-
 describe("Selected Movie View", () => {
     beforeEach(() => {
         cy.loadMain()
     });
-// cy.intercept({}) - sad path 400s
+
     it("should alert the user of a 404 error", () => {
         cy.get("#1")
             .click()
@@ -15,55 +12,19 @@ describe("Selected Movie View", () => {
                 message: "Something went wrong. Please try again later."
             }
             })
-        // cy.visit('http://localhost:3000')
-        //     .get("#1")
-        //     .click()
-            // cy.get("#1")
-            //   .click()
         cy.get("h2")
             .contains("Something went wrong. Please try again later.")
     })
-// cy.intercept({}) - sad path 500s
+
     it("should alert the user of a 500 error", () => {
         cy.get("#1")
             .click()
         cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v1/movies/1", {
             statusCode: 500
-            // body: {
-            //     message: "Our servers are currently down. Please try again."
-            // }
             })
-        // cy.visit('http://localhost:3000')
-        //     .get("#1")
-        //     .click()
         cy.get("h2")
             .contains("Our servers are currently down. Please try again.")
     })
-// cy.intercept({}) - loading 
-    // it("should tell user when the page is loading", () => {
-    //     cy.get("#1")
-    //         .click().should("include", "/1")
-    //     cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v1/movies/1", {
-    //             movie: {
-    //                 id: 1, 
-    //                 title: "Fake Movie Title", 
-    //                 poster_path: "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg", 
-    //                 backdrop_path: "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg", 
-    //                 release_date: "2019-12-04", 
-    //                 overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!", 
-    //                 average_rating: 6, 
-    //                 genres: ["Drama"], 
-    //                 budget:63000000, 
-    //                 revenue:100853753, 
-    //                 runtime:139, 
-    //                 tagline: "It's a movie!" 
-    //             }
-    //         })
-    //     cy.get("h2")
-    //         .contains("Loading...")
-    // })
-
-    //Happy path test for 201 fetch call
 
     it("should show the user the page's title", () => {
         cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v2/movies/1", {
