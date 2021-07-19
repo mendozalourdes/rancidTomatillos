aa8ef3bb8dd06e932dcfe333c6507caa2f7ed025
lourdesmendoza <lourd.mendoza@gmail.com>
2021-07-19 14:36:33 -0600

    Adjust final tests to make sure they are passing, fix indentation

--- cypress/integration/searchPageView_spec.js
+++ cypress/integration/searchPageView_spec.js
@@ -18,7 +18,7 @@ describe("User visits the search page view", () => {
 
   it("should be able to search for a movie given an input", () => {
     cy.get("input")
-      .type("Bill")
+      .type("Rogue")
       .get('.search-button').click()
       .get(".movie-container")
       .get("img")
@@ -29,7 +29,7 @@ describe("User visits the search page view", () => {
 
   it("should show no results if input does not match film selection", () => {
     cy.get("input")
-      .type("The Simpsons")
+      .type("The Sopranos")
       .get('.search-button').click()
       .get(".movie-container")
       .get("img")
@@ -86,74 +86,4 @@ describe("User visits the search page view", () => {
       .should("have.length", 41);
   });
 });
-
-
-
-// describe('User visits the search page view', () => {
-//     beforeEach(() => {
-//       cy.fixture('movies').then((movies) => {
-//         cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies', { body: movies }).as('movies')
-//       })
-//       cy.visit('http://localhost:3000/search')
-//     })
-
-// it('Should show the user a page title and directive text', () => {
-// cy.contains('h1', 'Rancid Tomatillos')
-// });
-
-// it('should have a search input and button on the page', () => {
-// cy.get('form').find('input')
-//   .get('form').find('button')
-// })
-
-// it('should be able to search for a movie given an input', () => {
-//     cy.get('input').type('Bill')
-//       .get('.movie-container')
-//       // .children().should('have.length', 1)
-//       // .get('img').should('have.class', 'movie-poster') 
-//       // .get('button').contains('More Info')
-//   })
-
-//   it('should show no results if input does not match film selection', () => {
-//     cy.get('input').type('The Simpsons')
-//       .get('.movie-container').children().should('have.length', 0)
-    
-//   })
-
-//   it('should show an error message if the input does not match film selection', () => {
-//     cy.get('input').type('The Simpsons')
-//       .get('.movie-container').children().should('have.length', 0)
-//     cy.get('h1')
-//         .contains("Sorry, we couldn't find any movies to match your search. Please try again!")
-    
-//   })
-
-//   it('should clear input after user clicks on the more info button', () => {
-//     cy.get('input').type('Mulan')
-//       .get('.movie-container').children().should('have.length', 1)
-//       .get('img').should('have.class', 'movie-poster') 
-//       .get('button').contains('More Info').click()
-//     cy.get('input').should('be.empty')
-    
-//   })
-
-//   it('should clear input after user clicks on the movie poster', () => {
-//     cy.get('input').type('Mulan')
-//       .get('.movie-container').children().should('have.length', 1)
-//       .get('img').should('have.class', 'movie-poster').click({force: true})
-//     cy.get('input').should('be.empty')
-    
-//   })
-
-//   it('should display all movies if user clicks on search button with empty input', () => {
-//     cy.get('input').should('be.empty')
-//     cy.get('form').find('button').click()
-//      .location('pathname').should('eq', '/search')
-//      .get('.movie-container').children().should('have.length', 41)
- 
-    
-//   })
-
-
-//     }); 
 
